2b345f7c091f1da86b018f941ce26f8f38fbfbb8
Amusa Paulos <amusaabayomipaul99@gmail.com>
2022-06-15 15:15:20 +0100

    added winning stripe and dialog

--- dist/index.js
+++ dist/index.js
@@ -1,10 +1,150 @@
 var playerInputs = document.querySelectorAll('.player-input');
 var error = document.querySelector('.error');
 var info = document.querySelector('.info');
+var winInfoPopup = document.querySelector('.winner-info-popup');
+var startButton = document.querySelector('.start');
+var replayButton = document.querySelector('.replay');
 var getAllTickTaks = function () {
     var all_tick_tack_boxes = [];
     document.querySelectorAll('.tto-box').forEach(function (tickTak) { return all_tick_tack_boxes.push(tickTak); });
     return all_tick_tack_boxes;
+};
+var startGame = function (el) {
+    playerInputs.forEach(function (input) { input.disabled = false; });
+    el.innerText = 'Pause';
+    el.removeEventListener('click', function () { return startGame(el); });
+    el.addEventListener('click', function () { return pauseGame(el); });
+};
+var pauseGame = function (el) {
+    playerInputs.forEach(function (input) { input.disabled = true; });
+    el.innerText = 'Resume';
+    el.addEventListener('click', function () { return startGame(el); });
+    el.removeEventListener('click', function () { return pauseGame(el); });
+};
+replayButton.addEventListener('click', function (el) {
+    getAllTickTaks().forEach(function (tickTak) { return tickTak.querySelector('span').className = ''; });
+    playerInputs.forEach(function (input) { input.disabled = true; input.value = ''; });
+    var curTarget = el.currentTarget;
+    curTarget.disabled = true;
+    pauseGame(startButton);
+    winInfoPopup.style.display = 'none';
+    info.querySelector('span').innerHTML = '';
+    var removeWinClass = document.querySelector('div[class*="win-"]').classList.toString().split(' ');
+    removeWinClass.splice(-1, 1);
+    document.querySelector('div[class*="win-"]').className = removeWinClass.join(' ');
+});
+startButton.addEventListener('click', function (e) { return startGame(e.currentTarget); });
+var checkWin = function () {
+    var all_tick_tack_boxes = getAllTickTaks();
+    var win = false;
+    var result = [false, -1, ''];
+    var _loop_1 = function (i) {
+        var checkRow = [];
+        if (i === 0) {
+            checkRow.push(all_tick_tack_boxes[0].querySelector('span').className);
+            if (checkRow[0] === '')
+                return "continue";
+            all_tick_tack_boxes.forEach(function (tickTak, index) {
+                if (index === 1 || (index === 2 && win != false)) {
+                    win = checkRow.includes(tickTak.querySelector('span').className) ? true : false;
+                }
+            });
+            if (win) {
+                result = [win, i, checkRow[0]];
+                return "break";
+            }
+        }
+        else if (i === 1) {
+            checkRow.push(all_tick_tack_boxes[3].querySelector('span').className);
+            if (checkRow[0] === '')
+                return "continue";
+            all_tick_tack_boxes.forEach(function (tickTak, index) {
+                if (index === 4 || (index === 5 && win != false)) {
+                    win = checkRow.includes(tickTak.querySelector('span').className) ? true : false;
+                }
+            });
+            if (win) {
+                result = [win, i, checkRow[0]];
+                return "break";
+            }
+        }
+        else if (i === 2) {
+            checkRow.push(all_tick_tack_boxes[6].querySelector('span').className);
+            if (checkRow[0] === '')
+                return "continue";
+            all_tick_tack_boxes.forEach(function (tickTak, index) {
+                if (index === 7 || (index === 8 && win != false)) {
+                    win = checkRow.includes(tickTak.querySelector('span').className) ? true : false;
+                }
+            });
+            if (win) {
+                result = [win, i, checkRow[0]];
+                return "break";
+            }
+        }
+        else if (i === 3) {
+            checkRow.push(all_tick_tack_boxes[2].querySelector('span').className);
+            if (checkRow[0] === '')
+                return "continue";
+            all_tick_tack_boxes.forEach(function (tickTak, index) {
+                if (index === 5 || (index === 8 && win != false)) {
+                    win = checkRow.includes(tickTak.querySelector('span').className) ? true : false;
+                }
+            });
+            if (win) {
+                result = [win, i, checkRow[0]];
+                return "break";
+            }
+        }
+        else if (i === 4) {
+            checkRow.push(all_tick_tack_boxes[6].querySelector('span').className);
+            if (checkRow[0] === '')
+                return "continue";
+            all_tick_tack_boxes.forEach(function (tickTak, index) {
+                if (index === 0 || (index === 4 && win != false)) {
+                    win = checkRow.includes(tickTak.querySelector('span').className) ? true : false;
+                }
+            });
+            if (win) {
+                result = [win, i, checkRow[0]];
+                return "break";
+            }
+        }
+        else if (i === 5) {
+            checkRow.push(all_tick_tack_boxes[0].querySelector('span').className);
+            if (checkRow[0] === '')
+                return "continue";
+            all_tick_tack_boxes.forEach(function (tickTak, index) {
+                if (index === 4 || (index === 8 && win != false)) {
+                    win = checkRow.includes(tickTak.querySelector('span').className) ? true : false;
+                }
+            });
+            if (win) {
+                result = [win, i, checkRow[0]];
+                return "break";
+            }
+        }
+        else if (i === 6) {
+            checkRow.push(all_tick_tack_boxes[2].querySelector('span').className);
+            if (checkRow[0] === '')
+                return "continue";
+            all_tick_tack_boxes.forEach(function (tickTak, index) {
+                if (index === 4 || (index === 6 && win != false)) {
+                    win = checkRow.includes(tickTak.querySelector('span').className) ? true : false;
+                }
+            });
+            if (win) {
+                result = [win, i, checkRow[0]];
+                return "break";
+            }
+        }
+    };
+    for (var i = 0; i < 7; i++) {
+        var state_1 = _loop_1(i);
+        if (state_1 === "break")
+            break;
+    }
+    return result;
 };
 var attachPlayEvent = function (e) {
     e.addEventListener('keyup', function (e) {
@@ -40,6 +180,34 @@ var attachPlayEvent = function (e) {
                     targetEl.value = '';
                     playerInputs.forEach(function (input) { return input.disabled = false; });
                     targetEl.disabled = true;
+                    playerInputs.forEach(function (input) { return input.focus(); });
+                    var _a = checkWin(), win = _a[0], row = _a[1], player = _a[2];
+                    console.log(checkWin());
+                    if (win) {
+                        // info.querySelector('span').innerHTML = 'Player ' + player + ' wins';
+                        playerInputs.forEach(function (input) { return input.disabled = true; });
+                        replayButton.disabled = false;
+                        if (row === 0) {
+                            document.querySelectorAll('.tto-inner')[0].classList.add('win-0');
+                        }
+                        else if (row === 1) {
+                            document.querySelectorAll('.tto-inner')[1].classList.add('win-1');
+                        }
+                        else if (row === 2) {
+                            document.querySelectorAll('.tto-inner')[2].classList.add('win-2');
+                        }
+                        else if (row === 3) {
+                            getAllTickTaks()[2].classList.add('win-3');
+                        }
+                        else if (row === 4) {
+                            getAllTickTaks()[6].classList.add('win-4');
+                        }
+                        else if (row === 5) {
+                            getAllTickTaks()[4].classList.add('win-5');
+                        }
+                        winInfoPopup.style.display = 'flex';
+                        winInfoPopup.querySelector('.inner div:nth-child(2)').innerHTML = player == 'o' ? '1' : '2';
+                    }
                 }
             });
         }
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,140 @@
 const playerInputs: NodeListOf<HTMLInputElement> = document.querySelectorAll('.player-input');
 const error: HTMLElement = document.querySelector('.error');
 const info: HTMLElement = document.querySelector('.info');
+const winInfoPopup: HTMLElement = document.querySelector('.winner-info-popup');
+const startButton: HTMLButtonElement = document.querySelector('.start');
+const replayButton: HTMLButtonElement = document.querySelector('.replay');
 
 const getAllTickTaks = (): Array<Element> => {
     let all_tick_tack_boxes: Array<Element> = []
     document.querySelectorAll('.tto-box').forEach(tickTak => all_tick_tack_boxes.push(tickTak));
     return all_tick_tack_boxes;
+}
+
+const startGame = (el: HTMLButtonElement): void => {
+    playerInputs.forEach(input => {input.disabled = false});
+    el.innerText = 'Pause';
+    el.removeEventListener('click', () => startGame(el));
+    el.addEventListener('click', () => pauseGame(el));
+}
+
+const pauseGame = (el: HTMLButtonElement): void => {
+    playerInputs.forEach(input => {input.disabled = true});
+    el.innerText = 'Resume';
+    el.addEventListener('click', () => startGame(el));
+    el.removeEventListener('click', () => pauseGame(el));
+}
+
+replayButton.addEventListener('click', el => {
+    getAllTickTaks().forEach(tickTak => tickTak.querySelector('span').className = '');
+    playerInputs.forEach(input => {input.disabled = true; input.value = ''});
+    const curTarget = el.currentTarget as HTMLButtonElement;
+    curTarget.disabled = true;  
+    pauseGame(startButton);  
+    winInfoPopup.style.display = 'none';
+    info.querySelector('span').innerHTML = '';
+    const removeWinClass: string[] = document.querySelector('div[class*="win-"]').classList.toString().split(' ')
+    removeWinClass.splice(-1, 1);
+    document.querySelector('div[class*="win-"]').className = removeWinClass.join(' ');
+})
+
+startButton.addEventListener('click', e => startGame(e.currentTarget as HTMLButtonElement));
+
+const checkWin = (): [boolean, number, string] => {
+    const all_tick_tack_boxes: Array<Element> = getAllTickTaks();
+    let win: boolean = false;
+    let result: [boolean, number, string] = [false, -1, ''];
+    for (let i = 0; i < 7; i++) {
+        const checkRow: string[] = [];
+        if (i === 0) {
+            checkRow.push(all_tick_tack_boxes[0].querySelector('span').className);
+            if (checkRow[0] === '') continue; 
+            all_tick_tack_boxes.forEach((tickTak: Element, index: number): void => {
+                if (index === 1 || (index === 2 && win != false)) {
+                    win = checkRow.includes(tickTak.querySelector('span').className) ? true : false
+                }
+            });
+            if (win) {
+                result = [win, i, checkRow[0]];
+                break;
+            }
+        }else if (i === 1) {
+            checkRow.push(all_tick_tack_boxes[3].querySelector('span').className);
+            if (checkRow[0] === '') continue; 
+            all_tick_tack_boxes.forEach((tickTak: Element, index: number): void => {
+                if (index === 4 || (index === 5 &&  win != false)) {
+                    win = checkRow.includes(tickTak.querySelector('span').className) ? true : false
+                }
+            });
+            if (win) {
+                result = [win, i, checkRow[0]];
+                break;
+            }
+        }else if (i === 2) {
+            checkRow.push(all_tick_tack_boxes[6].querySelector('span').className);
+            if (checkRow[0] === '') continue; 
+            all_tick_tack_boxes.forEach((tickTak: Element, index: number): void => {
+                if (index === 7 || (index === 8 &&  win != false)) {
+                    win = checkRow.includes(tickTak.querySelector('span').className) ? true : false
+                }
+            });
+            if (win) {
+                result = [win, i, checkRow[0]];
+                break;
+            }
+        }else if (i === 3) {
+            checkRow.push(all_tick_tack_boxes[2].querySelector('span').className);
+            if (checkRow[0] === '') continue; 
+            all_tick_tack_boxes.forEach((tickTak: Element, index: number): void => {
+                if (index === 5 || (index === 8 &&  win != false)) {
+                    win = checkRow.includes(tickTak.querySelector('span').className) ? true : false
+                }
+            });
+            if (win) {
+                result = [win, i, checkRow[0]];
+                break;
+            }
+        }else if (i === 4) {
+            checkRow.push(all_tick_tack_boxes[6].querySelector('span').className);
+            if (checkRow[0] === '') continue; 
+            all_tick_tack_boxes.forEach((tickTak: Element, index: number): void => {
+                if (index === 0 || (index === 4 &&  win != false)) {
+                    win = checkRow.includes(tickTak.querySelector('span').className) ? true : false
+                }
+            });
+            if (win) {
+                result = [win, i, checkRow[0]];
+                break;
+            }
+        }else if (i === 5) {
+            checkRow.push(all_tick_tack_boxes[0].querySelector('span').className);
+            if (checkRow[0] === '') continue; 
+            all_tick_tack_boxes.forEach((tickTak: Element, index: number): void => {
+                if (index === 4 || (index === 8 &&  win != false)) {
+                    win = checkRow.includes(tickTak.querySelector('span').className) ? true : false
+                }
+            });
+            if (win) {
+                result = [win, i, checkRow[0]];
+                break;
+            }
+        }else if (i === 6) {
+            checkRow.push(all_tick_tack_boxes[2].querySelector('span').className);
+            if (checkRow[0] === '') continue; 
+            all_tick_tack_boxes.forEach((tickTak: Element, index: number): void => {
+                if (index === 4 || (index === 6 &&  win != false)) {
+                    win = checkRow.includes(tickTak.querySelector('span').className) ? true : false
+                }
+            });
+            if (win) {
+                result = [win, i, checkRow[0]];
+                break;
+            }
+        }
+        
+    }
+
+    return result;
 }
 
 const attachPlayEvent = (e: HTMLInputElement): void => {
@@ -50,6 +179,33 @@ const attachPlayEvent = (e: HTMLInputElement): void => {
 
                     playerInputs.forEach(input => input.disabled = false);
                     targetEl.disabled = true;
+                    playerInputs.forEach(input => input.focus());
+
+                    const [win, row, player] = checkWin();
+                    console.log(checkWin())
+                    if (win) {
+                        // info.querySelector('span').innerHTML = 'Player ' + player + ' wins';
+                        playerInputs.forEach(input => input.disabled = true);
+                        replayButton.disabled = false;
+
+                        if (row === 0) {
+                            document.querySelectorAll('.tto-inner')[0].classList.add('win-0');
+                        }else if (row === 1) {
+                            document.querySelectorAll('.tto-inner')[1].classList.add('win-1');
+                        }else if (row === 2) {
+                            document.querySelectorAll('.tto-inner')[2].classList.add('win-2');
+                        }else if (row === 3) {
+                            getAllTickTaks()[2].classList.add('win-3');
+                        }else if (row === 4) {
+                            getAllTickTaks()[6].classList.add('win-4');
+                        }else if (row === 5) {
+                            getAllTickTaks()[4].classList.add('win-5');
+                        }
+
+                        winInfoPopup.style.display = 'flex';
+                        winInfoPopup.querySelector('.inner div:nth-child(2)').innerHTML = player == 'o' ? '1' : '2';
+                    }                      
+
                 }
             })       
         }
